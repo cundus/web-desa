edf37b1eec1c4446e529ac718c7059e94e336de6
sundus <sundusardian@gmail.com>
2023-04-15 13:31:08 +0700

    add router + layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./layout/RootLayout";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+         {
+            index: true,
+            element: <Home />,
+         },
+      ],
+   },
+]);
 
 const App = () => {
-   return <div>App</div>;
+   return <RouterProvider router={router} />;
 };
 
 export default App;
